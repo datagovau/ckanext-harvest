f97658d257fe9fa30cf4415513c7359a74995e78
gvonness <greg@sitetour.com.au>
2015-12-18 16:31:54 +1100

    - Allowing control characters inside strings.

--- ckanext/harvest/harvesters/ckanharvester.py
+++ ckanext/harvest/harvesters/ckanharvester.py
@@ -305,7 +305,7 @@ class CKANHarvester(HarvesterBase):
 
         try:
             log.debug('Loading JSON...')
-            package_dict = json.loads(harvest_object.content.decode('string_escape'))
+            package_dict = json.loads(harvest_object.content, strict=False)
             log.debug('JSON loaded!')
 
             if package_dict.get('type') == 'harvest':
